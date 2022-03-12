36249071568b3e1204cb42eb7bc2189567fa2d00
Rhea <rheaazarraga@gmail.com>
2022-03-11 19:22:18 -0800

    add 2nd exercise solution

--- practice.py
+++ practice.py
@@ -1,9 +1,17 @@
 # Coding Rooms, Interactive Coding Exercises
 
-# Exercise 1:
+# Exercise 1: Printing
 
 # Write your code below this line 👇
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+
+# Exercise 2: Debugging Practice
+
+# Fix the code below 👇
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
 
