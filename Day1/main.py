a3ff1632a69b097c27db91c65ba4aef7931518d1
Rhea <rheaazarraga@gmail.com>
2022-03-11 19:45:15 -0800

    add string concatenation exercise

--- Day1/main.py
+++ Day1/main.py
@@ -2,3 +2,13 @@ print("Hello World! This is my first time using Python!")
 
 # print on a new line:
 print("Hello World!\nThis is my first time using Python!\nPython is cool!")
+##############################
+# string concatenation:
+str1 = "Hello "
+str2 = "Bita"
+print(str1 + str2)
+# OR add a separate empty string between "Hello" and "Bita"
+print("Hello" + " " + "Bita")
+# OR with a space after "Hello"/ before "Bita"
+print("Hello " + "Bita")
+##############################
