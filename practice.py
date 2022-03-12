f9ecda2bc01409ff8a85ba0dfe3bcced21c95587
Rhea <rheaazarraga@gmail.com>
2022-03-11 19:29:12 -0800

    add exercise 4 and solution

--- practice.py
+++ practice.py
@@ -1,7 +1,6 @@
 # Coding Rooms, Interactive Coding Exercises
 
 # Exercise 1: Printing
-
 # Write your code below this line 👇
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
@@ -17,6 +16,28 @@ print("New lines can be created with a backslash and n.")
 
 # Exercise 3: Input Function
 # Write a program that prints the number of characters in a user's name
-#Write your code below this line 👇
+# Write your code below this line 👇
 string = input("What is your name?")
 print(len(string))
+
+# Exercise 4: Variables
+# Write a program that switches the values stored in the variables a & b
+# Do not change the code on lines 27-28 & 41-42. Your program should work for different inputs e.g. any value of a & b
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+# Write your code below this line 👇
+c = a
+a = b
+b = c
+
+# Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
