83605861868a06716e29114594a19d8db74314b0
Rhea <rheaazarraga@gmail.com>
2022-03-11 19:25:04 -0800

    add instructions to problems, add 3rd exercise solution

--- practice.py
+++ practice.py
@@ -8,10 +8,15 @@ print("The function is declared like this:")
 print("print('what to print')")
 
 # Exercise 2: Debugging Practice
-
+# There are errors in all the lines of code. Fix the code so that it runs without errors
 # Fix the code below 👇
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
+# Exercise 3: Input Function
+# Write a program that prints the number of characters in a user's name
+#Write your code below this line 👇
+string = input("What is your name?")
+print(len(string))
