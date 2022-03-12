16a94dbc68e45705ccec24310462ca1b98f4f719
Rhea <rheaazarraga@gmail.com>
2022-03-11 19:13:25 -0800

    add file for 4 daily coding room exercises

---
+++ practice.py
@@ -0,0 +1,9 @@
+# Coding Rooms, Interactive Coding Exercises
+
+# Exercise 1:
+
+# Write your code below this line 👇
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
