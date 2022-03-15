08efeccce32b2bd1eb06dba6f3f82b51aed0717f
Rhea <rheaazarraga@gmail.com>
2022-03-14 21:17:29 -0700

    add starter code for 2nd exercise

--- Day2/practice.py
+++ Day2/practice.py
@@ -17,3 +17,12 @@ second_digit = int(two_digit_number[1])
 # 3. Add the 2 digits together
 print(first_digit + second_digit)
 
+# Exercise 2: Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+# The BMI is a measure of some's weight taking into account their height. e.g. If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
