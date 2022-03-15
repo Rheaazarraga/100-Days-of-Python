413505f2e607824da655b01edb479d2dbe504b45
Rhea <rheaazarraga@gmail.com>
2022-03-14 19:46:16 -0700

    add starting notes on data types

---
+++ Day2/main.py
@@ -0,0 +1,14 @@
+# Data Types: Strings, Integers, Floats, Booleans
+
+# String - print the index of "o"
+print("Hello"[4])
+
+# Integers/ Numbers - whole numbers, no decimal places
+print(123 + 456)
+# Large numbers like 1,000,000 can be written as
+print(1_000_000)
+
+# Float / Floating Point Number - numbers with decimals
+print(3.14159)
+
+# Boolean - True or False
