c812a7560fa4a75112c938ed8c4e03eff391f096
Rhea <rheaazarraga@gmail.com>
2022-03-15 20:37:42 -0700

    fix spacing

--- Day1/practice.py
+++ Day1/practice.py
@@ -6,6 +6,8 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
+# ------------------------------------------------------------
+
 # Exercise 2: Debugging Practice
 # There are errors in all the lines of code. Fix the code so that it runs without errors
 # Fix the code below 👇
@@ -14,11 +16,15 @@ print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
 
+# ------------------------------------------------------------
+
 # Exercise 3: Input Function
 # Write a program that prints the number of characters in a user's name
 # Write your code below this line 👇
 string = input("What is your name?")
 print(len(string))
+
+# ------------------------------------------------------------
 
 # Exercise 4: Variables
 # Write a program that switches the values stored in the variables a & b
