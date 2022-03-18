159c615ab49d0d5e3038ef03064b591ed864d91c
Rheaazarraga <rheaazarraga@gmail.com>
2022-03-17 22:02:16 -0700

    revise starter code, add example input

--- Day2/tip-calculator.py
+++ Day2/tip-calculator.py
@@ -1,8 +1,13 @@
-#If the bill was $150.00, split between 5 people, with 12% tip.
+# If the bill was $150.00, split between 5 people, with 12% tip.
 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
+# Each person should pay (150.00 / 5) * 1.12 = 33.6
+# Format the result to 2 decimal places = 33.60
 
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+# Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
-#Write your code below this line 👇
+# Example Input Welcome to the tip calculator!
+# What was the total bill? $124.56
+# How much tip would you like to give? 10, 12, or 15? 12
+# How many people to split the bill? 7
+
+# Write your code below this line 👇
