af657258b57d57dc6800d2eb0597697af7d27822
Rhea <rheaazarraga@gmail.com>
2022-03-18 20:37:52 -0700

    add replit exercise from 21dcc prep mod

---
+++ Day3/journey.py
@@ -0,0 +1,11 @@
+# fix this horrendous interpretation of don't stop believin' by Journey
+
+journey = """Just a small tone girl
+Leaving in a lonely whirl
+She took the midnight tray going anywhere
+Just a seedy boy
+Bored and raised in South Detroit or something
+He took the midnight tray going anywhere"""
+
+#################################################
+
