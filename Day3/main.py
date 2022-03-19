e7de64840196796bac3fef357c39e3f3a04692df
Rhea <rheaazarraga@gmail.com>
2022-03-18 19:04:47 -0700

    notes on comparison operators, same as JS

--- Day3/main.py
+++ Day3/main.py
@@ -11,7 +11,10 @@ else:
 print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 
-if height > 120: # uses colon's instead of {}'s
+if height > 120: # testing condition uses colon's instead of {}'s
     print("You can ride the rollercoaster!") # within the scope of the if
 else:
     print("Sorry, you're not tall enough to ride.") # within the scope of the else
+
+# remember comparison operators : >x, <x, >=x, <=x, ==, !=
+# single = is assigning a value to a variable, double == check's the equality of values
