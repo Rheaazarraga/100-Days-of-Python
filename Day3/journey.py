e6d603c68a428d41b0d96290706959dc7f5d0e0c
Rhea <rheaazarraga@gmail.com>
2022-03-18 20:40:52 -0700

    add correction for journey lyrics using replace method

--- Day3/journey.py
+++ Day3/journey.py
@@ -9,3 +9,32 @@ He took the midnight tray going anywhere"""
 
 #################################################
 
+# using the replace() method which replaces a specified phrase with another specified phrase
+# long way:
+# journey = journey.replace("""Just a small tone girl
+# Leaving in a lonely whirl
+# She took the midnight tray going anywhere
+# Just a seedy boy
+# Bored and raised in South Detroit or something
+# He took the midnight tray going anywhere""", """Just a small town girl
+# Living in a lonely world
+# She took the midnight train going anywhere
+# Just a city boy
+# Born and raised in South Detroit
+# He took the midnight train going anywhere""")
+
+# print(journey)
+
+# OR - better way:
+
+journey= (journey
+.replace("tone", "town")
+.replace("Leaving", "Living")
+.replace("whirl", "world")
+.replace("tray", "train")
+.replace("seedy", "city")
+.replace("Bored", "born")
+.replace(" or something", "")
+         )
+
+print(journey)
