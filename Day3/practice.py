1688e3c8e777d7923d47cc20a5a37190d5fdf886
Rhea <rheaazarraga@gmail.com>
2022-03-18 20:34:10 -0700

    fix spacing to be consistent

--- Day3/practice.py
+++ Day3/practice.py
@@ -48,7 +48,7 @@ if bmi < 18.5:
     print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi < 25:
     print(f"Your BMI is {bmi}, you have a normal weight.")
-elif bmi <30:
+elif bmi < 30:
     print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi < 35:
     print(f"Your BMI is {bmi}, you are obese.")
