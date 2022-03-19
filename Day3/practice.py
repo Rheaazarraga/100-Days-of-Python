dd5cb5630a867c5a0e91428524df08b64eef3bcf
Rhea <rheaazarraga@gmail.com>
2022-03-18 19:54:41 -0700

    set up second exercise

--- Day3/practice.py
+++ Day3/practice.py
@@ -19,4 +19,25 @@ if number % 2 == 0:
 else:
     print("This is an odd number.")
 
+# ------------------------------------------------------------
+
+# Exercise 2:
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+# It should tell them the interpretation of their BMI based on the BMI value.
+
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m)
+
+# 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
 
