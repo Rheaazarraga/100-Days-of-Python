f4492a43f0f974aa5684cb928cf2f30c88e89570
Rhea <rheaazarraga@gmail.com>
2022-03-18 22:30:30 -0700

    add notes and modify rollercoaster example for multiple statements in succession

--- Day3/main.py
+++ Day3/main.py
@@ -37,10 +37,20 @@ if height > 120:
     print("You can ride the rollercoaster!")
     age = int(input("How old are you?"))
     if age < 12:
-        print("Please pay $5.")
+        bill = 5
+        print("Child tickets are $5.")
     elif age <=18: # catches everybody between the ages of 12-18
-        print("Please pay $7.")
+        bill = 7
+        print("Youth tickets are $7.")
     else:
-        print("Please pay $12.")
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want a photo taken? Y or N: ")
+    if wants_photo == "Y":
+    # Add $3 to their bill
+        bill += 3
+
+    print(f"Your final bill is ${bill}") #note the indentation
 else:
     print("Sorry, you're not tall enough to ride.")
