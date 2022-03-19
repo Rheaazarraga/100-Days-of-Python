0f4f18ae68eb901c116e98c63434674166b420b9
Rhea <rheaazarraga@gmail.com>
2022-03-18 19:19:16 -0700

    add solution for first exercise

--- Day3/practice.py
+++ Day3/practice.py
@@ -14,5 +14,9 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 # Write your code below this line 👇
+if number % 2 == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
 
 
