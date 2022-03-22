71154eac90aeab6d3dece0fac24273fd13056489
Rhea <rheaazarraga@gmail.com>
2022-03-21 20:19:23 -0700

    set up next exercise

--- Day3/practice.py
+++ Day3/practice.py
@@ -78,3 +78,23 @@ if (year % 4 == 0 or year % 400 == 0) and year % 100 != 0:
     print("Leap year.")
 else:
     print("Not leap year.")
+
+# ------------------------------------------------------------
+
+# Exercise 4: Build a pizza order program
+# Based on a user's order, work out their final bill.
+# Small Pizza: $15
+# Medium Pizza: $20
+# Large Pizza: $25
+# Pepperoni for Small Pizza: +$2
+# Pepperoni for Medium or Large Pizza: +$3
+# Extra cheese for any size pizza: + $1
+
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
