f786254bfc54fb7f0ff356e35414f45444b165cf
Rhea <rheaazarraga@gmail.com>
2022-03-21 21:19:26 -0700

    add solution for Python Pizza challenge

--- Day3/practice.py
+++ Day3/practice.py
@@ -83,9 +83,11 @@ else:
 
 # Exercise 4: Build a pizza order program
 # Based on a user's order, work out their final bill.
+
 # Small Pizza: $15
 # Medium Pizza: $20
 # Large Pizza: $25
+
 # Pepperoni for Small Pizza: +$2
 # Pepperoni for Medium or Large Pizza: +$3
 # Extra cheese for any size pizza: + $1
@@ -98,3 +100,26 @@ extra_cheese = input("Do you want extra cheese? Y or N ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+# S = 15
+# M = 20
+# L = 25
+bill = 0
+
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+else:
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill +=3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your final bill is: ${bill}.")
