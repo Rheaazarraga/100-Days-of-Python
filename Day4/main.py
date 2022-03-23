19c5bbbbd5ecf424c55dfe68f1d4749e8ae4eae6
Rhea <rheaazarraga@gmail.com>
2022-03-22 18:51:36 -0700

    add notes on randomisation, deterministic, nondeterministic, and pseudorandom number generators

---
+++ Day4/main.py
@@ -0,0 +1,5 @@
+# Randomisation
+
+# deterministic - like computers, performs repeatable actions in a predictable way - pseudorandom number generator -  produced by an algorithm that generates a series of bits that appear unpredictable, but in fact are computed from an algorithm
+# nondeterministic - a nondeterministic function may return different results every time it is called, even when the same input values are provided - a truly random number is a number selected from a range with each number in the range having equal and completely unpredictable chance of selection
+# python uses the Mersenne Twister pseudorandom number generator (PRNG)
