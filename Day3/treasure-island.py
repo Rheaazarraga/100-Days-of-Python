0de9c8d5d383d4253dab87221fd9f6ec28c2426a
Rhea <rheaazarraga@gmail.com>
2022-03-22 17:36:57 -0700

    add to adventure story

--- Day3/treasure-island.py
+++ Day3/treasure-island.py
@@ -23,3 +23,15 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
+
+crossroad = input("You are at a crossroad. Which path will you take? Type left or right: ")
+if crossroad == "left":
+    print("You have stumbled upon a murky swamp surrounded by a forest.")
+    nextChoice = input("Will you trench through the swamp or go through the forest? Type swamp or forest: ")
+    if nextChoice == "swamp":
+        print("You feel something swimming beside you but can't see through the water...\nYou try to swim faster in a panick, but the thing pulls you into the water with it. You drowned to death.")
+    else:
+        print("You race through the trees in the dark forest, and see a mysterious hut ahead with the windows lit up.\n As you approach the hut, you notice 3 different colored doors.")
+
+else:
+    print("The end of the path leads to a cliff. You take a step forward to see what lies below. The ground quickly collapses beneath you and you fall to your death.")
