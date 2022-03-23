fbbcb5db6fd40759c752c1a47c9956ae67e157e4
Rhea <rheaazarraga@gmail.com>
2022-03-22 17:06:34 -0700

    add python lhl 21dcc prep lesson

---
+++ Day3/storyString.py
@@ -0,0 +1,17 @@
+# LHL python 21dcc prep work
+# Challenge: Word Game/ Mad Libs
+# Remember to use:
+# Multi-line strings
+# User input with `input('...')`
+# The `.replace('phrase', 'new phrase')` method
+# The humble `print('...')` statement
+
+storyString = """Hi mom,
+
+Just writing to tell you that I've quit my job as a OCCUPATION and I'm moving to COUNTRY. The truth is, I've always been passionate about PLURAL_NOUN, and COUNTRY is the best place in the world to build a career around them. I'll need to start small-- At first, all I'll be allowed to do is to VERB near them, but when people see how ADJECTIVE I can be, I'm sure to rise to the top.
+
+Don't worry about me, and tell dad to take good care of my PERSONAL_ITEM. I'll be sure to call every HOLIDAY.
+
+Love,
+
+NAME"""
