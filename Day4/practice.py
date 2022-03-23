49194f2909c373fb61d55e4f20c4ea8980e10c86
Rhea <rheaazarraga@gmail.com>
2022-03-22 21:13:25 -0700

    set up second exercise

--- Day4/practice.py
+++ Day4/practice.py
@@ -18,3 +18,22 @@ if random_side == 1:
     print("Heads")
 else:
     print("Tails")
+
+# ------------------------------------------------------------
+
+# Exercise 2: Banker Roulette
+# write a program that will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+# ** Important: You are not allowed to use the choice() function.
+# Hint: You might need the help of the len() function.
+
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
