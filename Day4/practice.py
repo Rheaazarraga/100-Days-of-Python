16c7fe402faef50b21cf77ccf167faf74fc59572
Rhea <rheaazarraga@gmail.com>
2022-03-25 20:13:01 -0700

    set up 3rd challenge

--- Day4/practice.py
+++ Day4/practice.py
@@ -65,3 +65,24 @@ print(person_paying + " is going to buy the meal today!")
 
 # This is to try and simulate the coordinates on a real map.
 # Write a program that allows you to mark a square on the map using a two-digit system. The first digit is the vertical column number and the second digit is the horizontal row number.
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+
+
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
