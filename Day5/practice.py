6392b71732a4aa3f715e85767b17df7aa602adb0
Rhea <rheaazarraga@gmail.com>
2022-03-28 19:39:03 -0700

    add starter code

--- Day5/practice.py
+++ Day5/practice.py
@@ -45,3 +45,13 @@ print(average_height_of_students)
 # **Important** you are not allowed to use the max or min functions. The output words must match the example. i.e
 # Output: "The highest score in the class is: x"
 
+# Hint: Think about the logic before writing code. How can you compare numbers against each other to see which one is larger?
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
