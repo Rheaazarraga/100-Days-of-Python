5484fda9377e6e3d8a68e8ca6fbffe0b1d6c4da1
Rhea <rheaazarraga@gmail.com>
2022-03-28 18:10:21 -0700

    refactor code, take use cases into acct - if user chooses a number outside of 0, 1, and 2, cut down on nested conditionals

--- Day4/rock-paper-scissors.py
+++ Day4/rock-paper-scissors.py
@@ -33,27 +33,38 @@ users_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 
 computers_choice = random.randint(0, 2)
 print(computers_choice)
 
-if users_choice == 0:
-  print(rock)
-  if computers_choice == 1:
-    print(paper + "The computer wins")
-  elif computers_choice == 2:
-    print(scissors + "The player wins!")
-  else:
-    print(rock + "It's a draw!")
-elif users_choice == 1:
-  print(paper)
-  if computers_choice == 2:
-    print(scissors + "The computer wins")
-  elif computers_choice == 0:
-    print(rock + "The computer wins")
-  else:
-    print(paper + "It's a draw!")
-else:
-  print(scissors)
-  if computers_choice == 0:
-    print(rock + "The computer wins")
-  elif computers_choice == 1:
-    print(paper + "The player wins!")
-  else:
-    print(scissors + "It's a draw!")
+# if users_choice == 0:
+#   print(rock)
+#   if computers_choice == 1:
+#     print(paper + "The computer wins")
+#   elif computers_choice == 2:
+#     print(scissors + "The player wins!")
+#   else:
+#     print(rock + "It's a draw!")
+# elif users_choice == 1:
+#   print(paper)
+#   if computers_choice == 2:
+#     print(scissors + "The computer wins")
+#   elif computers_choice == 0:
+#     print(rock + "The computer wins")
+#   else:
+#     print(paper + "It's a draw!")
+# else:
+#   print(scissors)
+#   if computers_choice == 0:
+#     print(rock + "The computer wins")
+#   elif computers_choice == 1:
+#     print(paper + "The player wins!")
+#   else:
+#     print(scissors + "It's a draw!")
+
+if users_choice >=3 or users_choice <0:
+  print("You entered an invalid number, you lose")
+elif users_choice == 0 and computers_choice == 2:
+  print(f"{rock}\n{scissors} The computer chose scissors. You win! :)")
+elif computers_choice == 0 and users_choice == 2:
+  print(f"{scissors}\n{rock} The computer chose rock, you lose")
+elif computers_choice > users_choice:
+  print("You lose")
+elif computers_choice == users_choice:
+  print("It's a draw!")
