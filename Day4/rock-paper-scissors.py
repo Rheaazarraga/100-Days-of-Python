61200e2b5452e43ce8418d6da4a073984017218e
Rhea <rheaazarraga@gmail.com>
2022-03-28 18:31:50 -0700

    add logic for all possible outcomes for gameplay, as well as implementing a list for ascii image printing and ensuring condition checks are placed appropriately

--- Day4/rock-paper-scissors.py
+++ Day4/rock-paper-scissors.py
@@ -27,44 +27,26 @@ scissors = '''
 
 #Write your code below this line 👇
 import random
+game_choices = [rock, paper, scissors]
 
 users_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
-computers_choice = random.randint(0, 2)
-print(computers_choice)
-
-# if users_choice == 0:
-#   print(rock)
-#   if computers_choice == 1:
-#     print(paper + "The computer wins")
-#   elif computers_choice == 2:
-#     print(scissors + "The player wins!")
-#   else:
-#     print(rock + "It's a draw!")
-# elif users_choice == 1:
-#   print(paper)
-#   if computers_choice == 2:
-#     print(scissors + "The computer wins")
-#   elif computers_choice == 0:
-#     print(rock + "The computer wins")
-#   else:
-#     print(paper + "It's a draw!")
-# else:
-#   print(scissors)
-#   if computers_choice == 0:
-#     print(rock + "The computer wins")
-#   elif computers_choice == 1:
-#     print(paper + "The player wins!")
-#   else:
-#     print(scissors + "It's a draw!")
-
 if users_choice >=3 or users_choice <0:
   print("You entered an invalid number, you lose")
-elif users_choice == 0 and computers_choice == 2:
-  print(f"{rock}\n{scissors} The computer chose scissors. You win! :)")
+else:
+  print(game_choices[users_choice])
+
+computers_choice = random.randint(0, 2)
+print("The computer chose:")
+print(game_choices[computers_choice])
+
+if users_choice == 0 and computers_choice == 2:
+  print(f" You win!")
 elif computers_choice == 0 and users_choice == 2:
-  print(f"{scissors}\n{rock} The computer chose rock, you lose")
+  print(f"You lose")
 elif computers_choice > users_choice:
   print("You lose")
+elif users_choice > computers_choice:
+  print("You win!")
 elif computers_choice == users_choice:
   print("It's a draw!")
