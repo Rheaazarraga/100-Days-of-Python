7b7934b23b65693b041bb58bac17432e73adf093
Rhea <rheaazarraga@gmail.com>
2022-03-28 20:25:38 -0700

    add notes and examples on the range function

--- Day5/main.py
+++ Day5/main.py
@@ -8,3 +8,21 @@ for fruit in fruits:
     print(fruit + " Pie")
     print(fruits) # prints the list and iterates 3 times because there's 3 items in the list
 print(fruits) # prints the list once because it's outside the loop
+
+##############################
+
+# For loops with the range function
+# if you want to generate a range of numbers to loop through
+for number in range(1, 10): # *does not include 10*
+    print(number)
+# by default, the range function will step through all numbers from the start to the end and increment by 1
+# otherwise, you can add a third parameter indicating how much you want to increment (step) by
+for number in range(1, 11, 3):
+    print(number) # outputs 1, 4, 7, 10
+
+##############################
+
+total = 0 # accumulator
+for number in range(1, 101): # for 1 - 100
+    total += number # add every number in the range to the total
+print(total) # = 5050
