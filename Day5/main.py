bd70e91313ff9df78c6ec739990c6983e03212a5
Rhea <rheaazarraga@gmail.com>
2022-03-28 18:50:57 -0700

    add notes on for loops

---
+++ Day5/main.py
@@ -0,0 +1,10 @@
+# For Loop
+# for item in list_of_items:
+   #Do something to each item
+
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    #print(fruit)
+    print(fruit + " Pie")
+    print(fruits) # prints the list and iterates 3 times because there's 3 items in the list
+print(fruits) # prints the list once because it's outside the loop
