9e30be8a6ac15093a1bbaa147558847df798e05f
Rhea <rheaazarraga@gmail.com>
2022-04-12 19:04:59 -0700

    set up third challenge

--- Day5/practice.py
+++ Day5/practice.py
@@ -60,3 +60,13 @@ for score in student_scores:
     if score > highest_score:
         highest_score = score
 print(f"The highest score is: {highest_score}")
+
+# ------------------------------------------------------------
+
+# Exercise 3: Adding Evens
+# Write a program that calculates the sum of all the even numbers from 1 to 100. Thus, the first even number would be 2 and the last one is 100:
+# i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+# **Important** there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+# Hint: you will need to use the range() function in any of the solutions.
+
+# Write your code below this row 👇
